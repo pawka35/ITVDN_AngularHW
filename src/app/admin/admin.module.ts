cf3fd5c4c918527ee6930773bf95cc2ef3d769e7
skywalker35 <skywalker35@yandex.ru>
2020-01-25 19:08:24 +0300

    add routing

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AdminPanelComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
